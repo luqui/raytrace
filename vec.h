a0914818bcae6efe65d10ca0d14f21bac9866991
Luke Palmer <lrpalmer@gmail.com>
2014-06-19 22:52:10 -0700

    More in the vec lib.

--- vec.h
+++ vec.h
@@ -1,14 +1,66 @@
-class vec {
-    double x, y, z;
+#ifndef __VEC_H__
+#define __VEC_H__
+
+#include <cmath>
+
+struct vec;
+vec operator+ (const vec&, const vec&);
+vec operator- (const vec&, const vec&);
+vec operator* (double, const vec&);
+vec operator/ (const vec&, double);
+double operator* (const vec&, const vec&);
+
+struct vec {
 public:
+    double x, y, z;
     vec(double x, double y, double z) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
-    vec() : vec(0,0,0) { }
+    vec() : x(0), y(0), z(0) { }
+
+    double norm2() {
+        return x*x + y*y + z*z;
+    }
+
+    double norm() {
+        return std::sqrt(norm2());
+    }
+
+    vec unit() {
+        double length = norm();
+        if (length == 0) {
+            return *this;
+        }
+        else {
+            return *this/length;
+        }
+    }
+
+    static inline vec reflect(const vec& v, const vec& norm) {
+        return v - (2*v*norm)*norm;
+    }
 };
 
-inline vec operator* (const vec& a, const vec& b) {
-    return vec(a.x*b.x, a.y*b.y, c.x*c.y);
+inline vec operator+ (const vec& a, const vec& b) {
+    return vec(a.x+b.x, a.y+b.y, a.z+b.z);
 }
+
+inline vec operator- (const vec& a, const vec& b) {
+    return vec(a.x-b.x, a.y-b.y, a.z-b.z);
+}
+
+inline vec operator* (double a, const vec& v) {
+    return vec(a*v.x, a*v.y, a*v.z);
+}
+
+inline vec operator/ (const vec& v, double b) {
+    return (1/b)*v;
+}
+
+inline double operator* (const vec& a, const vec& b) {
+    return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+#endif
